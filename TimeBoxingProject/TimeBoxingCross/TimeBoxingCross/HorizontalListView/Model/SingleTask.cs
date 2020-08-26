7675d6f21d0dfe2874dd518701d32f98b7d59a3b
mr.david.r <mr.david.r@web.de>
2020-08-26 18:24:44 +0200

    First Commit - TimeboxingProject init

---
+++ TimeBoxingProject/TimeBoxingCross/TimeBoxingCross/HorizontalListView/Model/SingleTask.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TimeBoxingCross.HorizontalListView.Model
+{
+    public class SingleTask
+    {
+        public string Name { get; set; }
+    }
+}
---
+++ TimeBoxingProject/TimeBoxingCross/TimeBoxingCross/HorizontalListView/ViewModel/HorizontalListViewContainerViewModel.cs
@@ -0,0 +1,55 @@
+﻿using MvvmHelpers;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using TimeBoxingCross.HorizontalListView.Model;
+
+namespace TimeBoxingCross.HorizontalListView.ViewModel
+{
+    public class HorizontalListViewContainerViewModel : BaseViewModel
+    {
+
+        #region constructor
+
+
+        public HorizontalListViewContainerViewModel()
+        {
+            TaskCollection = new ObservableCollection<SingleTask>() { new SingleTask() { Name = "Tst" }, new SingleTask() { Name = "Tst2" }, new SingleTask() { Name = "Tst3" }, new SingleTask() { Name = "Tst4" }, new SingleTask() { Name = "Tst5" } };
+        }
+
+        #endregion 
+
+
+
+        #region properties
+
+        private ObservableCollection<SingleTask> _taskCollection;
+        public ObservableCollection<SingleTask> TaskCollection
+        {
+            get { return _taskCollection; }
+            set
+            {
+                if(_taskCollection != value)
+                {
+                    _taskCollection = value;
+                    OnPropertyChanged(nameof(TaskCollection));
+                }
+            }
+        }
+
+
+        #endregion
+
+        #region methods
+
+        
+
+
+        #endregion
+
+        #region commands
+        #endregion
+
+    }
+}
